9f3bd58f3a0515335763a373490eec4a831ecc69
IkerSobron <iker.sobron@ehu.eus>
2022-02-23 14:32:32 +0100

    He comentado una linea

--- app/src/main/java/com/iker/framents_test/SecondActivity.java
+++ app/src/main/java/com/iker/framents_test/SecondActivity.java
@@ -12,7 +12,7 @@ public class SecondActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Context c;
+//        Context c;
         AlumnInfoFragment elotro = (AlumnInfoFragment)getSupportFragmentManager().findFragmentById(R.id.segundoFragment);
         String n = getIntent().getStringExtra("nombre");
         String a = getIntent().getStringExtra("apellido");
